ea07731338d127150574275d313c7e870daf8aea
yakusugi <masterplan_life@protonmail.com>
2025-04-21 22:17:04 +0900

    Apr 21st MainContainerActivity

--- app/src/main/java/com/numosophy/activity/MainContainerActivity.kt
+++ app/src/main/java/com/numosophy/activity/MainContainerActivity.kt
@@ -3,16 +3,44 @@ package com.numosophy.activity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.numosophy.R
+import com.numosophy.databinding.ActivityMainContainerBinding
+import com.numosophy.fragment.DashboardFragment
+import com.numosophy.fragment.ExportFragment
+import com.numosophy.fragment.GroupFragment
+import com.numosophy.fragment.SalesFragment
+import com.numosophy.fragment.SettingsFragment
 import com.numosophy.utility.NumosophyDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainContainerActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainContainerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_container)
+        binding = ActivityMainContainerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main_container)
+        replaceFragment(DashboardFragment())
+
+        binding.bottomNavView.setOnItemReselectedListener {
+            when(it.itemId) {
+                R.id.dash -> replaceFragment(DashboardFragment())
+                R.id.sales -> replaceFragment(SalesFragment())
+                R.id.group -> replaceFragment(GroupFragment())
+                R.id.export -> replaceFragment(ExportFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
+
 
         val db = NumosophyDatabase.getDatabase(this)
         val userDao = db.userDao()
@@ -22,5 +50,12 @@ class MainContainerActivity : AppCompatActivity() {
             val userCount = userDao.getUserCount() // Query to check database existence
             Log.d("RoomTest", "Database initialized. Current users count: $userCount")
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
